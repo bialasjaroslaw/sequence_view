5568518a3c6ba79e4284f3e98dc823c0a1a53997
bialasjaroslaw <bialas.jaroslaw@gmail.com>
2023-12-30 14:18:27 +0100

    Format code

--- common.cc
+++ common.cc
@@ -1,207 +1,194 @@
-#include <sequence_view/common.hpp>
-
-#include <gtest/gtest.h>
-#include <gtest/gtest-matchers.h>
+#include <fmt/format.h>
 #include <gmock/gmock-more-matchers.h>
+#include <gtest/gtest-matchers.h>
+#include <gtest/gtest.h>
 
 #include <numeric>
-#include <fmt/format.h>
+#include <sequence_view/common.hpp>
 
 using namespace ::testing;
 
-void init_range(uint64_t* ptr, uint64_t len){
-    std::iota(ptr, ptr + len, 0);
+void init_range(uint64_t* ptr, uint64_t len) { std::iota(ptr, ptr + len, 0); }
+
+TEST(ViewHelpers, ValidateStep) {
+  std::vector<int64_t> input{-2, -1, 0, 1, 2};
+  std::vector<int64_t> expected{-2, -1, 1, 1, 2};
+
+  for (auto idx = 0; idx < input.size(); ++idx)
+    EXPECT_THAT(SeqView::View<uint64_t>::validate_step(input[idx]),
+                Eq(expected[idx]))
+        << fmt::format("Failed for {}", input[idx]);
 }
 
-TEST(ViewHelpers, ValidateStep)
-{
-    std::vector<int64_t> input{-2, -1, 0, 1, 2};
-    std::vector<int64_t> expected{-2, -1, 1, 1, 2};
+TEST(ViewHelpers, Elements) {
+  std::vector<uint64_t> input_size{0, 0,  1,  1,  9, 10, 11, 12, 13,
+                                   9, 10, 11, 12, 9, 9,  10, 11, 12};
+  std::vector<int64_t> input_step{0, 1, 1, 2, 3,  3,  3,  3,  3,
+                                  2, 2, 2, 2, -2, -3, -3, -3, -3};
+  std::vector<int64_t> expected{0, 0, 1, 1, 3, 4, 4, 4, 5,
+                                5, 5, 6, 6, 5, 3, 4, 4, 4};
 
-    for(auto idx = 0 ; idx < input.size() ; ++idx)
-        EXPECT_THAT(SeqView::View<uint64_t>::validate_step(input[idx]), Eq(expected[idx])) << fmt::format("Failed for {}", input[idx]);
+  for (auto idx = 0; idx < input_size.size(); ++idx)
+    EXPECT_THAT(
+        SeqView::View<uint64_t>::elements(input_size[idx], input_step[idx]),
+        Eq(expected[idx]))
+        << fmt::format("Failed for {}/{}", input_size[idx], input_step[idx]);
 }
 
-TEST(ViewHelpers, Elements)
-{
-    std::vector<uint64_t> input_size{0, 0, 1, 1, 9, 10, 11, 12, 13, 9, 10, 11, 12,  9,  9, 10, 11, 12};
-    std::vector<int64_t>  input_step{0, 1, 1, 2, 3,  3,  3,  3,  3, 2,  2,  2,  2, -2, -3, -3, -3, -3};
-    std::vector<int64_t>    expected{0, 0, 1, 1, 3,  4,  4,  4,  5, 5,  5,  6,  6,  5,  3,  4,  4,  4};
+TEST(ViewHelpers, LastPtr) {
+  uint16_t data = 0;
+  auto ptr = &data;
+  auto PC = [=](uint16_t value) { return ptr + value; };
+  std::vector<uint64_t> input_size{0, 0,  1,  1,  9, 10, 11, 12, 13,
+                                   9, 10, 11, 12, 9, 9,  10, 11, 12};
+  std::vector<int64_t> input_step{0, 1, 1, 2, 3,  3,  3,  3,  3,
+                                  2, 2, 2, 2, -2, -3, -3, -3, -3};
+  // expected is smallest multiple of step larger or equal size
+  std::vector<uint16_t*> expected{
+      PC(0),  PC(0),  PC(1),  PC(2),  PC(9),  PC(12), PC(12), PC(12), PC(15),
+      PC(10), PC(10), PC(12), PC(12), PC(10), PC(9),  PC(12), PC(12), PC(12)};
 
-    for(auto idx = 0 ; idx < input_size.size() ; ++idx)
-        EXPECT_THAT(SeqView::View<uint64_t>::elements(input_size[idx], input_step[idx]), Eq(expected[idx])) << fmt::format("Failed for {}/{}", input_size[idx], input_step[idx]);
+  for (auto idx = 0; idx < input_size.size(); ++idx) {
+    EXPECT_THAT(SeqView::View<uint16_t>::last_ptr(ptr, input_size[idx],
+                                                  input_step[idx]),
+                Eq(expected[idx]))
+        << fmt::format("Failed for {}/{}/{}", fmt::ptr(ptr), input_size[idx],
+                       input_step[idx]);
+  }
 }
 
-TEST(ViewHelpers, LastPtr)
-{
-    uint16_t data = 0;
-    auto ptr = &data;
-    auto PC = [=](uint16_t value){
-        return ptr + value;
-    };
-    std::vector<uint64_t> input_size{    0,     0,        1,      1,      9,    10, 
-                                        11,    12,       13,      9,     10,    11, 
-                                        12,     9,        9,     10,     11,    12};
-    std::vector<int64_t>  input_step{    0,     1,        1,      2,      3,     3,  
-                                         3,     3,        3,      2,      2,     2,  
-                                         2,    -2,       -3,     -3,     -3,    -3};
-                                         // expected is smallest multiple of step larger or equal size
-    std::vector<uint16_t*>  expected{PC(0),   PC(0),  PC(1),  PC(2),  PC(9), PC(12),
-                                     PC(12), PC(12), PC(15), PC(10), PC(10), PC(12),
-                                     PC(12), PC(10),  PC(9), PC(12), PC(12), PC(12)};
+TEST(ViewHelpers, BasePtr) {
+  uint16_t data = 0;
+  auto ptr = &data;
+  auto PC = [=](uint16_t value) { return ptr + value; };
+  std::vector<uint64_t> input_size{0, 0,  1,  1,  9, 10, 11, 12, 13,
+                                   9, 10, 11, 12, 9, 9,  10, 11, 12};
+  std::vector<int64_t> input_step{0, 1, 1, 2, 3,  3,  3,  3,  3,
+                                  2, 2, 2, 2, -2, -3, -3, -3, -3};
+  // expected is smallest multiple of step larger or equal size
+  std::vector<uint16_t*> expected{PC(0), PC(0),  PC(0), PC(0),  PC(0),  PC(0),
+                                  PC(0), PC(0),  PC(0), PC(0),  PC(0),  PC(0),
+                                  PC(0), PC(10), PC(9), PC(12), PC(12), PC(12)};
 
-    for(auto idx = 0 ; idx < input_size.size() ; ++idx){
-        EXPECT_THAT(SeqView::View<uint16_t>::last_ptr(ptr, input_size[idx], input_step[idx]), Eq(expected[idx])) << fmt::format("Failed for {}/{}/{}", fmt::ptr(ptr), input_size[idx], input_step[idx]);
-    }
+  for (auto idx = 0; idx < input_size.size(); ++idx) {
+    EXPECT_THAT(SeqView::View<uint16_t>::base_ptr(ptr, input_size[idx],
+                                                  input_step[idx]),
+                Eq(expected[idx]))
+        << fmt::format("Failed for {}/{}/{}", fmt::ptr(ptr), input_size[idx],
+                       input_step[idx]);
+  }
 }
 
-TEST(ViewHelpers, BasePtr)
-{
-    uint16_t data = 0;
-    auto ptr = &data;
-    auto PC = [=](uint16_t value){
-        return ptr + value;
-    };
-    std::vector<uint64_t> input_size{    0,     0,        1,      1,      9,    10, 
-                                        11,    12,       13,      9,     10,    11, 
-                                        12,     9,        9,     10,     11,    12};
-    std::vector<int64_t>  input_step{    0,     1,        1,      2,      3,     3,  
-                                         3,     3,        3,      2,      2,     2,  
-                                         2,    -2,       -3,     -3,     -3,    -3};
-                                         // expected is smallest multiple of step larger or equal size
-    std::vector<uint16_t*>  expected{PC(0),   PC(0),  PC(0),  PC(0),  PC(0), PC(0),
-                                     PC(0),   PC(0),  PC(0),  PC(0),  PC(0), PC(0),
-                                     PC(0),   PC(10), PC(9), PC(12), PC(12), PC(12)};
+TEST(ViewHelpers, EndPtr) {
+  uint16_t data = 0;
+  auto ptr = &data;
+  auto PC = [=](uint16_t value) { return ptr + value; };
+  std::vector<uint64_t> input_size{0, 0,  1,  1,  9, 10, 11, 12, 13,
+                                   9, 10, 11, 12, 9, 9,  10, 11, 12};
+  std::vector<int64_t> input_step{0, 1, 1, 2, 3,  3,  3,  3,  3,
+                                  2, 2, 2, 2, -2, -3, -3, -3, -3};
+  // expected is smallest multiple of step larger or equal size
+  std::vector<uint16_t*> expected{
+      PC(0),  PC(0),  PC(1),  PC(2),  PC(9), PC(12), PC(12), PC(12), PC(15),
+      PC(10), PC(10), PC(12), PC(12), PC(0), PC(0),  PC(0),  PC(0),  PC(0)};
 
-    for(auto idx = 0 ; idx < input_size.size() ; ++idx){
-        EXPECT_THAT(SeqView::View<uint16_t>::base_ptr(ptr, input_size[idx], input_step[idx]), Eq(expected[idx])) << fmt::format("Failed for {}/{}/{}", fmt::ptr(ptr), input_size[idx], input_step[idx]);
-    }
+  for (auto idx = 0; idx < input_size.size(); ++idx) {
+    EXPECT_THAT(
+        SeqView::View<uint16_t>::end_ptr(ptr, input_size[idx], input_step[idx]),
+        Eq(expected[idx]))
+        << fmt::format("Failed for {}/{}/{}", fmt::ptr(ptr), input_size[idx],
+                       input_step[idx]);
+  }
 }
 
-TEST(ViewHelpers, EndPtr)
-{
-    uint16_t data = 0;
-    auto ptr = &data;
-    auto PC = [=](uint16_t value){
-        return ptr + value;
-    };
-    std::vector<uint64_t> input_size{    0,     0,        1,      1,      9,    10, 
-                                        11,    12,       13,      9,     10,    11, 
-                                        12,     9,        9,     10,     11,    12};
-    std::vector<int64_t>  input_step{    0,     1,        1,      2,      3,     3,  
-                                         3,     3,        3,      2,      2,     2,  
-                                         2,    -2,       -3,     -3,     -3,    -3};
-                                         // expected is smallest multiple of step larger or equal size
-    std::vector<uint16_t*>  expected{PC(0),   PC(0),  PC(1),  PC(2),  PC(9), PC(12),
-                                     PC(12), PC(12), PC(15), PC(10), PC(10), PC(12),
-                                     PC(12),  PC(0),  PC(0),  PC(0),  PC(0),  PC(0)};
-
-    for(auto idx = 0 ; idx < input_size.size() ; ++idx){
-        EXPECT_THAT(SeqView::View<uint16_t>::end_ptr(ptr, input_size[idx], input_step[idx]), Eq(expected[idx])) << fmt::format("Failed for {}/{}/{}", fmt::ptr(ptr), input_size[idx], input_step[idx]);
-    }
+TEST(Common, MakeRange) {
+  SeqView::Range rng(12, 23, 2);
+  EXPECT_THAT(rng.start, Eq(12));
+  EXPECT_THAT(rng.stop, Eq(23));
+  EXPECT_THAT(rng.step, Eq(2));
 }
 
-TEST(Common, MakeRange)
-{
-    SeqView::Range rng(12, 23, 2);
-    EXPECT_THAT(rng.start, Eq(12));
-    EXPECT_THAT(rng.stop, Eq(23));
-    EXPECT_THAT(rng.step, Eq(2));
+TEST(Common, MakeRangeDefaultStep) {
+  SeqView::Range rng(12, 23);
+  EXPECT_THAT(rng.start, Eq(12));
+  EXPECT_THAT(rng.stop, Eq(23));
+  EXPECT_THAT(rng.step, Eq(SeqView::STEP));
 }
 
-TEST(Common, MakeRangeDefaultStep)
-{
-    SeqView::Range rng(12, 23);
-    EXPECT_THAT(rng.start, Eq(12));
-    EXPECT_THAT(rng.stop, Eq(23));
-    EXPECT_THAT(rng.step, Eq(SeqView::STEP));
+TEST(Common, MakeStartRange) {
+  SeqView::StartRange rng(12, 2);
+  EXPECT_THAT(rng.start, Eq(12));
+  EXPECT_THAT(rng.stop, Eq(SeqView::END));
+  EXPECT_THAT(rng.step, Eq(2));
 }
 
-TEST(Common, MakeStartRange)
-{
-    SeqView::StartRange rng(12, 2);
-    EXPECT_THAT(rng.start, Eq(12));
-    EXPECT_THAT(rng.stop, Eq(SeqView::END));
-    EXPECT_THAT(rng.step, Eq(2));
+TEST(Common, MakeStartRangeDefaultStep) {
+  SeqView::StartRange rng(12);
+  EXPECT_THAT(rng.start, Eq(12));
+  EXPECT_THAT(rng.stop, Eq(SeqView::END));
+  EXPECT_THAT(rng.step, Eq(SeqView::STEP));
 }
 
-TEST(Common, MakeStartRangeDefaultStep)
-{
-    SeqView::StartRange rng(12);
-    EXPECT_THAT(rng.start, Eq(12));
-    EXPECT_THAT(rng.stop, Eq(SeqView::END));
-    EXPECT_THAT(rng.step, Eq(SeqView::STEP));
+TEST(Common, MakeStopRange) {
+  SeqView::StopRange rng(12, 2);
+  EXPECT_THAT(rng.start, Eq(SeqView::START));
+  EXPECT_THAT(rng.stop, Eq(12));
+  EXPECT_THAT(rng.step, Eq(2));
 }
 
-TEST(Common, MakeStopRange)
-{
-    SeqView::StopRange rng(12, 2);
-    EXPECT_THAT(rng.start, Eq(SeqView::START));
-    EXPECT_THAT(rng.stop, Eq(12));
-    EXPECT_THAT(rng.step, Eq(2));
+TEST(Common, MakeStopRangeDefaultStep) {
+  SeqView::StopRange rng(12);
+  EXPECT_THAT(rng.start, Eq(SeqView::START));
+  EXPECT_THAT(rng.stop, Eq(12));
+  EXPECT_THAT(rng.step, Eq(SeqView::STEP));
 }
 
-TEST(Common, MakeStopRangeDefaultStep)
-{
-    SeqView::StopRange rng(12);
-    EXPECT_THAT(rng.start, Eq(SeqView::START));
-    EXPECT_THAT(rng.stop, Eq(12));
-    EXPECT_THAT(rng.step, Eq(SeqView::STEP));
+TEST(Common, MakeStepRange) {
+  SeqView::StepRange rng(2);
+  EXPECT_THAT(rng.start, Eq(SeqView::START));
+  EXPECT_THAT(rng.stop, Eq(SeqView::END));
+  EXPECT_THAT(rng.step, Eq(2));
 }
 
-TEST(Common, MakeStepRange)
-{
-    SeqView::StepRange rng(2);
-    EXPECT_THAT(rng.start, Eq(SeqView::START));
-    EXPECT_THAT(rng.stop, Eq(SeqView::END));
-    EXPECT_THAT(rng.step, Eq(2));
+TEST(Common, Make1DView) {
+  uint64_t data[100];
+  SeqView::View view(data, 100);
+  EXPECT_THAT(view, SizeIs(100));
 }
 
-TEST(Common, Make1DView)
-{
-    uint64_t data[100];
-    SeqView::View view(data, 100);
-    EXPECT_THAT(view, SizeIs(100));
+TEST(Common, IterateAssignOver1DView) {
+  uint64_t data[100];
+  SeqView::View view(data, 100);
+  uint64_t num = 0;
+  for (auto& elem : view) elem = num++;
+  EXPECT_THAT(view, ElementsAreArray(data, 100));
 }
 
-TEST(Common, IterateAssignOver1DView)
-{
-    uint64_t data[100];
-    SeqView::View view(data, 100);
-    uint64_t num = 0;
-    for(auto& elem : view)
-        elem = num++;
-    EXPECT_THAT(view, ElementsAreArray(data, 100));
+TEST(Common, Make1DSubView) {
+  uint64_t data[100];
+  SeqView::View view(data, 100);
+  auto sub = view(SeqView::Range{10, 20, 2});
+  EXPECT_THAT(sub, SizeIs(5));
 }
 
-TEST(Common, Make1DSubView)
-{
-    uint64_t data[100];
-    SeqView::View view(data, 100);
-    auto sub = view(SeqView::Range{10, 20, 2});
-    EXPECT_THAT(sub, SizeIs(5));
+TEST(Common, IterateWithStep) {
+  uint64_t data[100];
+  init_range(data, 100);
+  SeqView::View view(data, 100);
+  auto sub = view(SeqView::Range{10, 20, 2});
+  EXPECT_THAT(sub, SizeIs(5));
+  for (int64_t idx = 10, view_idx = 0; idx < 20; idx += 2, ++view_idx) {
+    EXPECT_THAT(sub[view_idx], Eq(data[idx]));
+  }
 }
 
-TEST(Common, IterateWithStep)
-{
-    uint64_t data[100];
-    init_range(data, 100);
-    SeqView::View view(data, 100);
-    auto sub = view(SeqView::Range{10, 20, 2});
-    EXPECT_THAT(sub, SizeIs(5));
-    for(int64_t idx = 10, view_idx = 0 ; idx < 20 ; idx += 2, ++view_idx){
-        EXPECT_THAT(sub[view_idx], Eq(data[idx]));
-    }
-}
-
-TEST(Common, IterateWithNegativeStep)
-{
-    uint64_t data[100];
-    init_range(data, 100);
-    SeqView::View view(data, 100);
-    auto sub = view(SeqView::Range{10, 20, -2});
-    EXPECT_THAT(sub, SizeIs(5));
-    for(int64_t idx = 18, view_idx = 0 ; idx > 8 ; idx -= 2, ++view_idx){
-        EXPECT_THAT(sub[view_idx], Eq(data[idx]));
-    }
+TEST(Common, IterateWithNegativeStep) {
+  uint64_t data[100];
+  init_range(data, 100);
+  SeqView::View view(data, 100);
+  auto sub = view(SeqView::Range{10, 20, -2});
+  EXPECT_THAT(sub, SizeIs(5));
+  for (int64_t idx = 18, view_idx = 0; idx > 8; idx -= 2, ++view_idx) {
+    EXPECT_THAT(sub[view_idx], Eq(data[idx]));
+  }
 }
--- include/sequence_view/common.hpp
+++ include/sequence_view/common.hpp
@@ -1,212 +1,163 @@
 #pragma once
 
-#include <cstdint>
 #include <cmath>
+#include <cstdint>
 #include <limits>
 
-namespace SeqView{
-	using RangeT = int64_t;
-	constexpr RangeT START = 0;
-	constexpr RangeT END = std::numeric_limits<RangeT>::max();
-	constexpr RangeT STEP = 1;
+namespace SeqView {
+using RangeT = int64_t;
+constexpr RangeT START = 0;
+constexpr RangeT END = std::numeric_limits<RangeT>::max();
+constexpr RangeT STEP = 1;
 
-	struct Range{
-		Range(RangeT start, RangeT stop, RangeT step = STEP)
-		: start(start)
-		, stop(stop)
-		, step(step)
-		{}
-		RangeT start = START;
-		RangeT stop = END;
-		RangeT step = STEP;
-	};
+struct Range {
+  Range(RangeT start, RangeT stop, RangeT step = STEP)
+      : start(start), stop(stop), step(step) {}
+  RangeT start = START;
+  RangeT stop = END;
+  RangeT step = STEP;
+};
 
-	struct StepRange: Range{
-		StepRange(RangeT step) 
-		: Range(START, END, step)
-		{}
-	};
+struct StepRange : Range {
+  StepRange(RangeT step) : Range(START, END, step) {}
+};
 
-	struct StartRange: Range{
-		StartRange(RangeT start, RangeT step = STEP) 
-		: Range(start, END, step)
-		{}
-	};
+struct StartRange : Range {
+  StartRange(RangeT start, RangeT step = STEP) : Range(start, END, step) {}
+};
 
-	struct StopRange: Range{
-		StopRange(RangeT stop, RangeT step = STEP) 
-		: Range(START, stop, step)
-		{}
-	};
+struct StopRange : Range {
+  StopRange(RangeT stop, RangeT step = STEP) : Range(START, stop, step) {}
+};
 
-	template <typename T>
-	struct BaseIterator{
-		BaseIterator(T* data, int64_t step = STEP)
-		: _data(data)
-		, _step(step)
-		, _dstep(step < 0 ? step : 0)
-		{}
+template <typename T>
+struct BaseIterator {
+  BaseIterator(T* data, int64_t step = STEP)
+      : _data(data), _step(step), _dstep(step < 0 ? step : 0) {}
 
-		const T& operator*() const {
-			return *ptr();
-		}
+  const T& operator*() const { return *ptr(); }
 
-		const T* operator->() const{
-			return ptr();
-		}
-		
-		friend bool operator==(const BaseIterator<T>& lhs, const BaseIterator<T>& rhs){
-			return lhs._data == rhs._data;
-		}
+  const T* operator->() const { return ptr(); }
 
-		friend bool operator!=(const BaseIterator<T>& lhs, const BaseIterator<T>& rhs){
-			return lhs._data != rhs._data;
-		}
-		
-		BaseIterator& operator++(){
-			_data += _step;
-			return *this;
-		}
+  friend bool operator==(const BaseIterator<T>& lhs,
+                         const BaseIterator<T>& rhs) {
+    return lhs._data == rhs._data;
+  }
 
-		BaseIterator operator+(int steps){
-			return BaseIterator<T>(_data + _step * steps, _step);
-		}
-	protected:
-		T* ptr(){
-			return _data + _dstep;
-		}
-		const T* ptr() const{
-			return _data + _dstep;
-		}
-	private:
-		T* _data = nullptr;
-		int64_t _step = STEP;
-		int64_t _dstep = 0;
-	};
+  friend bool operator!=(const BaseIterator<T>& lhs,
+                         const BaseIterator<T>& rhs) {
+    return lhs._data != rhs._data;
+  }
 
-	template <typename T>
-	struct Iterator : BaseIterator<T>{
-		Iterator(T* data, int64_t step = STEP) 
-		: BaseIterator<T>(data, step)
-		{}
+  BaseIterator& operator++() {
+    _data += _step;
+    return *this;
+  }
 
-		T& operator*(){
-			return *BaseIterator<T>::ptr();
-		}
+  BaseIterator operator+(int steps) {
+    return BaseIterator<T>(_data + _step * steps, _step);
+  }
 
-		T* operator->(){
-			return BaseIterator<T>::ptr();
-		}
-	};
+ protected:
+  T* ptr() { return _data + _dstep; }
+  const T* ptr() const { return _data + _dstep; }
 
-	template <typename T>
-	struct ConstIterator : BaseIterator<T>{
-		ConstIterator(T* data, int64_t step = STEP) 
-		: BaseIterator<T>(data, step)
-		{}
-	};
+ private:
+  T* _data = nullptr;
+  int64_t _step = STEP;
+  int64_t _dstep = 0;
+};
 
-	template <typename T>
-	class View{
-	public:
-		using size_type = std::size_t;
-        using value_type = T;
-        using pointer = T*;
-        using const_pointer = T const*;
-        using iterator = Iterator<T>;
-        using const_iterator = ConstIterator<T>;
-        using reference = T&;
-        using const_reference = T const&;
+template <typename T>
+struct Iterator : BaseIterator<T> {
+  Iterator(T* data, int64_t step = STEP) : BaseIterator<T>(data, step) {}
 
-        constexpr static int64_t validate_step(int64_t step){
-        	if(step == 0)
-        		return 1;
-        	return step;
-        }
+  T& operator*() { return *BaseIterator<T>::ptr(); }
 
-        constexpr static uint64_t elements(uint64_t size, int64_t step){
-        	if(size == 0 || step == 0)
-        		return 0;
-        	step = std::abs(step);
-        	if(step > size)
-        		return 1;
-        	return (size - 1) / step + 1;
-        }
+  T* operator->() { return BaseIterator<T>::ptr(); }
+};
 
-        constexpr static pointer last_ptr(pointer ptr, uint64_t size, int64_t step){
-        	auto number_of_jumps = elements(size, step);
-        	auto first_outside_boundary = number_of_jumps * std::abs(step);
-        	return ptr + first_outside_boundary;
-        }
+template <typename T>
+struct ConstIterator : BaseIterator<T> {
+  ConstIterator(T* data, int64_t step = STEP) : BaseIterator<T>(data, step) {}
+};
 
-		constexpr static pointer base_ptr(pointer ptr, uint64_t size, int64_t step){
-        	if(step > 0)
-        		return ptr;
-        	return last_ptr(ptr, size, step);
-        }
+template <typename T>
+class View {
+ public:
+  using size_type = std::size_t;
+  using value_type = T;
+  using pointer = T*;
+  using const_pointer = T const*;
+  using iterator = Iterator<T>;
+  using const_iterator = ConstIterator<T>;
+  using reference = T&;
+  using const_reference = T const&;
 
-        constexpr static pointer end_ptr(pointer ptr, uint64_t size, int64_t step){
-        	if(step < 0)
-        		return ptr;
-        	return last_ptr(ptr, size, step);
-        }
+  constexpr static int64_t validate_step(int64_t step) {
+    if (step == 0) return 1;
+    return step;
+  }
 
-		View(pointer ptr, uint64_t size, int64_t step = 1)
-		: _ptr(base_ptr(ptr, size, step))
-		, _end(end_ptr(ptr, size, step))
-		, _step(validate_step(step))
-		, _size(elements(size, step))
-		{}
+  constexpr static uint64_t elements(uint64_t size, int64_t step) {
+    if (size == 0 || step == 0) return 0;
+    step = std::abs(step);
+    if (step > size) return 1;
+    return (size - 1) / step + 1;
+  }
 
-		uint64_t size() const{
-			return _size;
-		}
+  constexpr static pointer last_ptr(pointer ptr, uint64_t size, int64_t step) {
+    auto number_of_jumps = elements(size, step);
+    auto first_outside_boundary = number_of_jumps * std::abs(step);
+    return ptr + first_outside_boundary;
+  }
 
-		iterator begin(){
-			return iterator(_ptr, _step);
-		}
-		
-		iterator end(){
-			return iterator(_end, _step);
-		}
+  constexpr static pointer base_ptr(pointer ptr, uint64_t size, int64_t step) {
+    if (step > 0) return ptr;
+    return last_ptr(ptr, size, step);
+  }
 
-		const_iterator begin() const {
-			return const_iterator(_ptr, _step);
-		}
-		
-		const_iterator end() const {
-			return const_iterator(_end, _step);
-		}
+  constexpr static pointer end_ptr(pointer ptr, uint64_t size, int64_t step) {
+    if (step < 0) return ptr;
+    return last_ptr(ptr, size, step);
+  }
 
-		const_iterator cbegin() const {
-			return const_iterator(_ptr, _step);
-		}
-		const_iterator cend() const {
-			return const_iterator(_end, _step);
-		}
+  View(pointer ptr, uint64_t size, int64_t step = 1)
+      : _ptr(base_ptr(ptr, size, step)),
+        _end(end_ptr(ptr, size, step)),
+        _step(validate_step(step)),
+        _size(elements(size, step)) {}
 
-		View operator()(Range rng) {
-			return View(_ptr + rng.start, rng.stop - rng.start, rng.step);
-		}
+  uint64_t size() const { return _size; }
 
-		reference operator[](uint64_t idx){
-			return *element_at(idx);
-		}
+  iterator begin() { return iterator(_ptr, _step); }
 
-		const_reference operator[](uint64_t idx) const{
-			return* element_at(idx);
-		}
+  iterator end() { return iterator(_end, _step); }
 
-	protected:
+  const_iterator begin() const { return const_iterator(_ptr, _step); }
 
-		T* element_at(int idx) const {
-			return _ptr + idx * _step + (_step < 0 ? _step : 0);
-		}
+  const_iterator end() const { return const_iterator(_end, _step); }
 
-	private:
-		int64_t _step;
-		uint64_t _size = 0;
-		pointer _ptr = nullptr;
-		pointer _end = nullptr;
-	};
-}
+  const_iterator cbegin() const { return const_iterator(_ptr, _step); }
+  const_iterator cend() const { return const_iterator(_end, _step); }
+
+  View operator()(Range rng) {
+    return View(_ptr + rng.start, rng.stop - rng.start, rng.step);
+  }
+
+  reference operator[](uint64_t idx) { return *element_at(idx); }
+
+  const_reference operator[](uint64_t idx) const { return *element_at(idx); }
+
+ protected:
+  T* element_at(int idx) const {
+    return _ptr + idx * _step + (_step < 0 ? _step : 0);
+  }
+
+ private:
+  int64_t _step;
+  uint64_t _size = 0;
+  pointer _ptr = nullptr;
+  pointer _end = nullptr;
+};
+}  // namespace SeqView
