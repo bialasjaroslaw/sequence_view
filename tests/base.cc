0ea11999a6c454e94c96fda7b0401f75b393a1af
bialasjaroslaw <bialas.jaroslaw@gmail.com>
2024-01-07 22:03:47 +0100

    Add pre/post increment/decrement tests

--- tests/base.cc
+++ tests/base.cc
@@ -14,9 +14,85 @@ TEST(Iterator, DefaultCtor) {
 }
 
 TEST(Iterator, ComparisonChecksOfTwoDifferentIterators) {
-  int array[4];
+  int array[2];
   auto begin = SeqView::BaseIterator(array);
-  auto end = SeqView::BaseIterator(array + 4);
+  auto end = SeqView::BaseIterator(array + 2);
   EXPECT_THAT(begin, Ne(end));
   EXPECT_THAT(begin, Lt(end));
+}
+
+TEST(Iterator, PostIncrement) {
+  int array[2];
+  auto begin = SeqView::BaseIterator(array);
+  auto end = SeqView::BaseIterator(array + 2);
+  auto it = begin;
+  auto other = it++;
+  EXPECT_THAT(other, Eq(begin));
+  EXPECT_THAT(it, Ne(begin));
+  EXPECT_THAT(it, Gt(begin));
+  EXPECT_THAT(it, Ne(end));
+  EXPECT_THAT(it, Lt(end));
+
+  it++;
+
+  EXPECT_THAT(it, Ne(begin));
+  EXPECT_THAT(it, Gt(begin));
+  EXPECT_THAT(it, Eq(end));
+}
+
+TEST(Iterator, PreIncrement) {
+  int array[2];
+  auto begin = SeqView::BaseIterator(array);
+  auto end = SeqView::BaseIterator(array + 2);
+  auto it = begin;
+  auto other = ++it;
+  EXPECT_THAT(other, Eq(it));
+  EXPECT_THAT(it, Ne(begin));
+  EXPECT_THAT(it, Gt(begin));
+  EXPECT_THAT(it, Ne(end));
+  EXPECT_THAT(it, Lt(end));
+
+  ++it;
+
+  EXPECT_THAT(it, Ne(begin));
+  EXPECT_THAT(it, Gt(begin));
+  EXPECT_THAT(it, Eq(end));
+}
+
+TEST(Iterator, PostDecrement) {
+  int array[2];
+  auto begin = SeqView::BaseIterator(array);
+  auto end = SeqView::BaseIterator(array + 2);
+  auto it = end;
+  auto other = it--;
+  EXPECT_THAT(other, Eq(end));
+  EXPECT_THAT(it, Ne(end));
+  EXPECT_THAT(it, Lt(end));
+  EXPECT_THAT(it, Ne(begin));
+  EXPECT_THAT(it, Gt(begin));
+
+  it--;
+
+  EXPECT_THAT(it, Ne(end));
+  EXPECT_THAT(it, Lt(end));
+  EXPECT_THAT(it, Eq(begin));
+}
+
+TEST(Iterator, PreDecrement) {
+  int array[2];
+  auto begin = SeqView::BaseIterator(array);
+  auto end = SeqView::BaseIterator(array + 2);
+  auto it = end;
+  auto other = --it;
+  EXPECT_THAT(other, Eq(it));
+  EXPECT_THAT(it, Ne(end));
+  EXPECT_THAT(it, Lt(end));
+  EXPECT_THAT(it, Ne(begin));
+  EXPECT_THAT(it, Gt(begin));
+
+  --it;
+
+  EXPECT_THAT(it, Ne(end));
+  EXPECT_THAT(it, Lt(end));
+  EXPECT_THAT(it, Eq(begin));
 }
