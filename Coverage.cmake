f2e3a9c36f9df981c08336a5243c182b10a413e7
bialasjaroslaw <bialas.jaroslaw@gmail.com>
2024-01-09 20:15:18 +0100

    Enable code coverage only when available

--- Coverage.cmake
+++ Coverage.cmake
@@ -1,3 +1,14 @@
+function(check_coverage out_var)
+    set(oneValueArgs TARGET)
+    find_program(LCOV lcov)
+    find_program(GENHTML genhtml)
+    set(TOOLS_FOUND FALSE)
+    if(LCOV AND GENHTML)
+        set(TOOLS_FOUND TRUE)
+    endif()
+    set(${out_var} ${TOOLS_FOUND} PARENT_SCOPE)
+endfunction()
+
 macro(add_coverage)
     set(oneValueArgs TARGET)
     set(multiValueArgs IGNORE_LIST)
@@ -16,10 +27,11 @@ macro(add_coverage)
 
     add_custom_target(${COVERAGE_TARGET}_coverage
       # gather data
-      COMMAND ${LCOV} --directory . --capture --output-file ${COVERAGE_TARGET}_capture_coverage.info 
+      COMMAND ${LCOV} --directory . --capture --output-file ${COVERAGE_TARGET}_capture_coverage.info
       COMMAND ${LCOV} --extract ${COVERAGE_TARGET}_capture_coverage.info ${CMAKE_CURRENT_LIST_DIR}/\\* --output-file ${COVERAGE_TARGET}_extract_coverage.info
       COMMAND ${LCOV} --remove ${COVERAGE_TARGET}_extract_coverage.info ${IGNORE_LIST} ${CMAKE_CURRENT_BINARY_DIR}/\\* --output-file ${COVERAGE_TARGET}_remove_coverage.info
       # generate report
       COMMAND ${GENHTML} --demangle-cpp -o ${COVERAGE_TARGET}_coverage ${COVERAGE_TARGET}_remove_coverage.info
       WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
+    add_dependencies(${COVERAGE_TARGET}_coverage ${COVERAGE_TARGET})
 endmacro()
